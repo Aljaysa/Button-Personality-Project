3c9bbe6a9a0b0fdb60d42ccf5b313e705db8c1e4
julliana-ramos <123857181+julliana-ramos@users.noreply.github.com>
2026-01-30 15:58:26 -0700

    Finished most of my button

--- index.js
+++ index.js
@@ -63,3 +63,79 @@ jasonButton.addEventListener("click", () => {
 
 });
 
+
+// /////////////////////////////////////////////
+//           JULLIANA ANIMATIONS              //
+// /////////////////////////////////////////////
+
+const jullianaAssets = [
+    document.getElementById("jullianaLaptop"),
+    document.getElementById("jullianaPiano"),
+    document.getElementById("jullianaPerson"),
+    document.getElementById("jullianaPolaroid"),
+    document.getElementById("jullianaVanity"),
+    document.getElementById("jullianaMic"),
+    document.getElementById("jullianaLuggage")
+  ];
+  
+  const jullianaFlash = document.getElementById("jullianaBTNAnimation");
+  
+  const jullianaButton = document.getElementById("jullianaButton");
+  const jasonBtn = document.getElementById("jasonButton");
+  const justinBtn = document.getElementById("justinButton");
+  
+  let jullianaAssetsVisible = false;
+  let currentSize = 0;
+  let unlocked = false;
+  
+  jullianaButton.classList.add("size-0");
+  
+  jullianaButton.addEventListener("click", () => {
+    if (!unlocked) {
+      jullianaButton.classList.remove(`size-${currentSize}`);
+      currentSize++;
+      jullianaButton.classList.add(`size-${currentSize}`);
+      if (currentSize >= 4) {
+        unlocked = true;
+        // Show assets and glow when button reaches full size
+        jullianaAssets.forEach((asset, i) => {
+          setTimeout(() => {
+            asset.classList.add("visible");
+          }, i * 120);
+        });
+        jullianaAssetsVisible = true;
+
+        jasonBtn.classList.add("glow");
+        justinBtn.classList.add("glow");
+        jullianaFlash.classList.add("visible");
+      }
+      return;
+    }
+  
+    // Once button at full size
+    jullianaButton.classList.add("active");
+    // Hide assets and glow
+    if (jullianaAssetsVisible) {
+      jullianaAssets.forEach(asset => {
+        asset.classList.remove("visible");
+      });
+      jullianaAssetsVisible = false;
+  
+      jasonBtn.classList.remove("glow");
+      justinBtn.classList.remove("glow");
+      jullianaFlash.classList.remove("visible");
+      // Show assets and glow on click
+    } else {
+      jullianaAssets.forEach((asset, i) => {
+        setTimeout(() => {
+          asset.classList.add("visible");
+        }, i * 120);
+      });
+      jullianaAssetsVisible = true;
+  
+      jasonBtn.classList.add("glow");
+      justinBtn.classList.add("glow");
+      jullianaFlash.classList.add("visible");
+    }
+  });
+  
